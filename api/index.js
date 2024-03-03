f7d80814c9c3e4a7a6d53843e182fb07943707e1
KvnKl007 <klakkent99@gmail.com>
2024-03-03 11:21:12 +0200

    Creating and connecting to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
+
+mongoose.connect(process.env.Mongo).then(() => {
+    console.log('Connected to MongoDB')
+}).catch((err) => {
+    console.log(err)
+})
 const app = express()
 
 app.listen(3030, () => {
-    console.log('Server started on port 3030?')
+    console.log('Server started on port 3030!')
 })
