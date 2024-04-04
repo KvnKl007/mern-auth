d0ff47eba9c552171ab4365dba727ce4c561ad75
KvnKl007 <klakkent99@gmail.com>
2024-04-04 11:18:00 +0200

    complete signup page functionallity

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user-model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utilis/error.js";
 
+
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -14,6 +15,6 @@ export const signup = async (req, res, next) => {
         await newUser.save();
         res.status(201).json({message: "User created"});
     } catch (error) {
-        next(error);
+        next(errorHandler);
     }
 };
