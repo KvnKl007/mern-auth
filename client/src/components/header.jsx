4a150b25394c5800f25dbddde77ad2875519d8bb
KvnKl007 <klakkent99@gmail.com>
2024-05-23 20:03:21 +0200

    fixing something

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
             <Link to='/'>
             <h1 className='font-bold'>Auth App</h1>
             </Link>
-            <ul className='flex gap-4'>
+            <ul className='flex gap-4 items-center'>
                 <Link to='/'><li>Home</li></Link>
                 <Link to='/about'><li>About</li></Link>
                 <Link to='/Profile'>
